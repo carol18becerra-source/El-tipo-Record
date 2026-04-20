83d7987191f70c7050dd69d33f5886fba3ace23d
Carolina <carol18becerra@gmail.com>
2026-04-20 19:05:02 +0200

    clase Persona creada como un record manualmente

--- el-tipo-record/src/main/java/com/example/App.java
+++ el-tipo-record/src/main/java/com/example/App.java
@@ -3,10 +3,11 @@ package com.example;
 public class App {
 
     static final int XX = 5;
+
     public static void main(String[] args) {
 
         // una clase final significa: que ninguna otra clase puede heredad de ella
         Persona persona = new Persona();
-        
+
     }
 }
